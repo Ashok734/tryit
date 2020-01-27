3ef38c73adc850b27d9747b3f4242b4f03f93b7c
Ashok Shah <you@example.com>
2020-01-27 22:45:47 +0545

    new file

---
+++ js/main.js
@@ -0,0 +1,81 @@
+
+
+// $('.ashok').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     autoplay:true,
+//     animateIn: 'fadeIn',
+//     animateOut: 'fadeOut',
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:1
+//         }
+//     }
+// })
+
+
+// $(window).on('scroll',function(){
+//         if ($(window).scrollTop()){
+//           $('nav').addClass('black');
+//         }
+//         else 
+//         {
+//             $('nav').removeClass('black');
+//         }
+//       })
+
+
+
+// $('.suraz').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     autoplay:true,
+//     animateIn: 'fadeIn',
+//     animateOut: 'fadeOut',
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:4
+//         }
+//     }
+// })
+
+// $('.tools').owlCarousel({
+//     center: true,
+//             loop:true,
+//             margin:30,
+//             nav:false,
+//             dots:true,
+//             autoplay: true,
+//             slideTransition: 'linear',
+//             autoplayTimeout: 4000,
+//             autoplaySpeed: 4000,
+//             autoplayHoverPause: true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:6
+//         }
+//     }
+// })
+
+// footer fixed
+
